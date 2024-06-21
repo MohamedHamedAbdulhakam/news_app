857cd9c1452d8ef82248761d2959beb9c64f0464
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-06-21 13:04:38 +0300

    homeview part1,2

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -13,10 +13,25 @@ class HomeView extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text("News",style: TextStyle(color: Colors.black),),
-              Text(" Cloud",style: TextStyle(color: Colors.orange),),
+              Text("Cloud",style: TextStyle(color: Colors.orange),),
           ],
         ),
         backgroundColor: Colors.transparent ,
+      ),
+      body: Container(
+        height: 200,
+        width: 400,
+       
+        decoration: BoxDecoration(
+           color: Colors.red,
+           borderRadius: BorderRadius.circular(15),
+           
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage('th.jpg'))
+          
+        ),
+        child: Center(child: Text("Buisness",style: TextStyle(color: Colors.deepOrange,fontSize: 30,fontWeight: FontWeight.bold,),),),
       ),
     );
   }
