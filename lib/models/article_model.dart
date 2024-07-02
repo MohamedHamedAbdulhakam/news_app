2fbc621c4858f1ec8e772a30bcdb9604f222db51
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-07-02 22:36:45 +0300

    more about json data

--- lib/models/article_model.dart
+++ lib/models/article_model.dart
@@ -2,6 +2,17 @@ class ArticleModel {
   final String? image;
   final String title;
   final String? subtitle;
+  final Source? source;
   ArticleModel(
-      {required this.image, required this.title, required this.subtitle});
+      { //required this.source,
+      required this.image,
+      required this.title,
+      required this.subtitle,
+      this.source});
+}
+
+class Source {
+  final String name;
+  final String id;
+  Source({required this.id, required this.name});
 }
